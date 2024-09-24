041ecd3580e2254415ed6e7e731292c4ca61408d
Mohammad Akbar <128262698+M-Ak21r@users.noreply.github.com>
2024-09-24 19:37:20 +0530

    completed the project

---
+++ main.py
@@ -0,0 +1,112 @@
+MENU = {
+    "espresso":{
+        "ingredients":{
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte":{
+        "ingredients":{
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino":{
+        "ingredients":{
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
+}
+
+running = True
+while running:
+    #TODO 1: Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+    prompt = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    #TODO 2: Turn off the Coffee Machine by entering “off” to the prompt
+    if prompt == "off":
+        running = False
+        exit()
+
+    #TODO 3: Print report
+    if prompt == "report":
+        for item in resources:
+           print(f"{item}: {resources[item]}")
+        exit()
+
+    #TODO 4: Check resources sufficient?
+    if prompt == "espresso":
+        if MENU["espresso"]["ingredients"]["water"] > resources["water"]:
+            print("Sorry, there is not enough water.")
+            exit()
+        elif MENU["espresso"]["ingredients"]["coffee"] > resources["coffee"]:
+            print("Sorry, there is not enough coffee.")
+            exit()
+
+    if prompt == "latte":
+        if MENU["latte"]["ingredients"]["water"] > resources["water"]:
+            print("Sorry, there is not enough water.")
+            exit()
+        elif MENU["latte"]["ingredients"]["milk"] > resources["milk"]:
+            print("Sorry, there is not enough milk.")
+            exit()
+        elif MENU["latte"]["ingredients"]["coffee"] > resources["coffee"]:
+            print("Sorry, there is not enough coffee.")
+            exit()
+
+    if prompt == "cappuccino":
+        if MENU["cappuccino"]["ingredients"]["water"] > resources["water"]:
+            print("Sorry, there is not enough water.")
+            exit()
+        elif MENU["cappuccino"]["ingredients"]["milk"] > resources["milk"]:
+            print("Sorry, there is not enough milk.")
+            exit()
+        elif MENU["cappuccino"]["ingredients"]["coffee"] > resources["coffee"]:
+            print("Sorry, there is not enough coffee.")
+            exit()
+
+    #TODO 5: Process coins.
+    penny = int(input("How many pennies?: "))
+    nickel = int(input("How many nickels?: "))
+    dime = int(input("How many dimes?: "))
+    quarter = int(input("How many quarters?: "))
+
+    totalMoney = (penny * 0.01) + (nickel * 0.05) + (dime * 0.10) + (quarter * 0.25)
+
+    #TODO 6: Check transaction successful?
+    if MENU[prompt]["cost"] > totalMoney:
+        print(f"Sorry, that's not enough money. Money refunded.")
+        exit()
+
+    elif MENU[prompt]["cost"] == totalMoney:
+        resources["money"] += MENU[prompt]["cost"]
+
+    elif MENU[prompt]["cost"] < totalMoney:
+        resources["money"] += MENU[prompt]["cost"]
+        change = totalMoney - MENU[prompt]["cost"]
+        print(f"Here is {change:.2f} in change.")
+
+    #TODO 7: Make Coffee.
+    if prompt == ("espresso" or "latte" or "cappuccino"):
+        resources["water"] -= MENU[prompt]["ingredients"]["water"]
+
+    if prompt == ("espresso" or "latte" or "cappuccino"):
+        resources["coffee"] -= MENU[prompt]["ingredients"]["coffee"]
+
+    if prompt == ("latte" or "cappuccino"):
+        resources["milk"] -= MENU[prompt]["ingredients"]["milk"]
+
+    print(f"Here is your {prompt}. Enjoy!")
